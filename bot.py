4108653ff85fc356a742be7772da36546049f501
nasriv <vnasri91@gmail.com>
2023-02-25 19:03:09 -0500

    initial push

---
+++ bot.py
@@ -0,0 +1,44 @@
+import discord
+from discord.ext import commands
+import os
+from dotenv import load_dotenv
+import random
+from scrape_util import *
+
+# If you are coding the bot on a local machine, use the python-dotenv pakcage to get variables stored in .env file of your project
+from dotenv import load_dotenv
+load_dotenv()
+
+description = '''An example bot to showcase the discord.ext.commands extension
+module.'''
+
+# instantiate discord client
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', description=description, intents=intents)
+
+# discord event to check when bot is online
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+    print('------')
+
+# set bot commands
+@bot.event
+async def on_message(message): 
+    # make sure bot doesn't respond to it's own messages to avoid infinite loop
+    if message.author == bot.user:
+        return  
+    
+    # lower case message
+    message_content = message.content.lower()
+
+    if message.channel.name == "dev_test_env":
+        if message.content.startswith(f'$hello'):
+            await message.channel.send(f'''Hello {str(message.author).split('#')[0]}! I'm here to help''')
+
+# get bot token from .env and run client
+# has to be at the end of the file
+bot.run(os.getenv('TOKEN'))
