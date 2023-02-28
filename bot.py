804f1c98f671e4aa57b998b49083e038907394f6
Vincent Nasri <vincent.nasri@cytiva.com>
2023-02-27 23:53:35 -0500

    update bot to collect from all members

--- bot.py
+++ bot.py
@@ -2,9 +2,9 @@ import discord
 from discord.ext import commands
 import os
 from dotenv import load_dotenv
-import time
 from utils import *
 import sqlite3
+import asyncio
 
 # If you are coding the bot on a local machine, use the python-dotenv pakcage to get variables stored in .env file of your project
 from dotenv import load_dotenv
@@ -42,6 +42,10 @@ async def on_message(message):
 
         # get trivia question
         if message.content.startswith(f'$trivia'):
+            
+            global answers
+            i=0
+
             result = get_question()
             for k, v in result.items():
                 if k == "q":
@@ -53,39 +57,40 @@ async def on_message(message):
                 else:
                     await message.channel.send(str(k)+": *"+str(v)+"*")
 
+            # collect trivia answers
             answers={}
-            for member in message.channel.members:
-                if member.bot:
-                    continue
+            for i in range(10):
                 try:
-                    message = await bot.wait_for('message', timeout=15.0, check=lambda m: m.author == member and not m.author.bot)
+                    message = await bot.wait_for('message', timeout=1)
                     if message.author.bot or message.author.name in answers:
                         continue
-                    answers[str(message.author.name)] = message.content
-                except:
-                    answers[message.author.name] = None
-            print(answers)
-            await message.channel.send("answer is.... "+result["correctAns"])
+                    # print(f"Submitting message: {message.content}")
+                    answers[str(message.author.name)] = message.content.lower()
+                except asyncio.TimeoutError:
+                    continue
+
+            # await asyncio.sleep(timeout)
+            await message.channel.send("Answer ....**"+result["correctAns"]+"**")
+
+            # print(f"submitted answer dictionary: {answers}")
 
             # check user answers and update db to add or remove points
-            for usr, ans in answers.items():
-                if ans == result["correctAns"]:
-                    print("db insert-->",usr, ans, result["correctAns"])
-                    add_score(usr, result["point value"])
+            for author, ans in answers.items():
+                print(author, ans)
+                if ans is None:
+                    continue
+                elif ans.lower() == result["correctAns"].lower():
+                    print("add point value")
+                    add_score(author, result["point value"])
                 else:
                     # if answer is wrong increment game played
-                    add_game_data(usr)
-
+                    print("wrong: increment question count")
+                    add_game_data(author)
 
 
         # get current scoreboard
-        if message.content == ('$scoreboard'):
-            await message.channel.send("------ Scoreboard ------")             
-            conn = sqlite3.connect("triviaBot.db")
-            c = conn.cursor()
-            for row in c.execute("select name, score, cast(numCorr as real)*100/q_played as '% Correct' from scoreboard order by score desc"):
-                await message.channel.send('\t'.join(map(str,row)))
-            conn.close()
+        if message.content == ('$scoreboard'):          
+            await message.channel.send('------ Scoreboard ------\n >>> {}'.format('\n'.join(get_scores())))
 
         # initialize db
         if message.content == ('$init'):
@@ -95,7 +100,7 @@ async def on_message(message):
             c = conn.cursor()
 
             # create initial table
-            c.execute("create table if not exists scoreboard (id text primary key,username text not null,name text not null,score integer,q_played integer,numCorr integer,numWrong integer,perCorrect real);")
+            c.execute("create table if not exists scoreboard (id text primary key,username text not null,name text not null,score integer,q_played integer,numCorr integer);")
             
             # get all members in channel
             members = bot.get_all_members()
@@ -103,7 +108,7 @@ async def on_message(message):
             for member in members:
                 # only return message if member not bot role
                 if not member.bot:
-                    c.execute(f"INSERT INTO scoreboard (id, username, name, score, q_played, numCorr, numWrong, perCorrect) VALUES (?,?,?,?,?,?,)", (str(member.id),str(member),str(member.name),0,0,0))
+                    c.execute(f"INSERT INTO scoreboard (id, username, name, score, q_played, numCorr) VALUES (?,?,?,?,?,?)", (str(member.id),str(member),str(member.name),0,0,0))
             conn.commit()
             conn.close()
             await message.channel.send("db initialized")
--- utils.py
+++ utils.py
@@ -5,28 +5,27 @@ import sqlite3
 def get_question():
     # pull question and return various string values in dict form for bot parsing
     try:
-        r = requests.get('https://opentdb.com/api.php?amount=1')
+        r = requests.get('https://opentdb.com/api.php?amount=1&type=multiple')
         json_response = r.json()['results'][0]
 
-        q_type = json_response['type']
+        # q_type = json_response['type']
         category = json_response['category']
         question = html.unescape(json_response['question'])
         diff = json_response['difficulty']
-        answer = json_response['correct_answer']
+        answer = html.unescape(json_response['correct_answer'])
 
-        if q_type == 'multiple':
-            all_choices = json_response['incorrect_answers']
-            all_choices.append(answer)
-            clean_choices = []
-            for item in all_choices:
-                # replace all html entitiy tags
-                clean_choices.append(html.unescape(item))
+        # if q_type == 'multiple':
+        all_choices = json_response['incorrect_answers']
+        all_choices.append(answer)
+        clean_choices = []
+        for item in all_choices:
+            # replace all html entitiy tags
+            clean_choices.append(html.unescape(item))
 
-            # merge all answers into single string for function return
-            all_choices = '\n'.join(clean_choices)
-        else:
-            all_choices = '\n'.join(['True', 'False'])
+        # merge all answers into single string for function return
+        all_choices = '\n'.join(clean_choices)
 
+        # set point value based on ques. difficulty
         if diff == 'easy':
             point_val = 1
         elif diff == 'medium':
@@ -56,10 +55,36 @@ def add_score(user, points):
 def add_game_data(user):
     conn = sqlite3.connect('triviaBot.db')
     c = conn.cursor()
-    c.execute("UPDATE scoreboard set q_played=q_played+1 where name=?",(user))
-    #BUG
+    c.execute("UPDATE scoreboard set q_played=q_played+1 where name=?",(user,))
     conn.commit()
     conn.close()
+
+def get_scores():
+        conn = sqlite3.connect("triviaBot.db")
+        c = conn.cursor()
+        result = c.execute(
+            '''select name as "Player", 
+                cast(score as text) as "Score", 
+                case when q_played = 0 then cast(0 as TEXT)
+                    else cast(round(cast(numCorr as REAL)/q_played*100,0) as TEXT) 
+                    end as "% Correct", 
+                cast(q_played as TEXT) as "Qs Answered" 
+                from scoreboard 
+                order by score desc''')
+        
+        colHeaders = tuple(map(lambda x: x[0], c.description))
+        response = result.fetchall()
+        response.insert(0,colHeaders)
+
+        scores = []
+
+        # convert from tuple to string
+        for item in response:
+            scores.append('\t'.join(item))
+
+        conn.close()
+
+        return scores
 
 def question_counter():
     pass
@@ -68,3 +93,5 @@ def db_init():
     pass
 
 #get_question()
+# ----- testing --------
+get_scores()
